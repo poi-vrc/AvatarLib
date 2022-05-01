e557b623e51ff48e9390cdedd4f76fb1d24be293
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2022-05-01 21:47:58 +0800

    Add worldconstraint utils

---
+++ Packages/com.chocopoi.vrc.avatarlib/Editor/WorldConstraint/WorldConstraintUtils.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Text.RegularExpressions;
+using VRC.SDK3.Avatars.ScriptableObjects;
+using UnityEngine.Animations;
+using Chocopoi.AvatarLib.Animations;
+
+namespace Chocopoi.AvatarLib.WorldConstraint
+{
+    /// <summary>
+    /// An utility class to create a WorldConstraint in an avatar
+    /// </summary>
+    public class WorldConstraintUtils
+    {
+        /// <summary>
+        /// Creates a world constraint
+        /// </summary>
+        /// <param name="item">The item to be applied a world constraint on</param>
+        /// <param name="root">Parent the world constraint to a Transform</param>
+        /// <param name="resetTarget">The reset target to reset the world constraint to</param>
+        /// <returns></returns>
+        private static WorldConstraint CreateWorldConstraint(GameObject item = null, Transform root = null, Transform resetTarget = null)
+        {
+            // World space
+
+            GameObject worldSpaceObj = new GameObject("WorldConstraint");
+
+            if (root != null)
+            {
+                worldSpaceObj.transform.SetParent(root);
+            }
+
+            GameObject worldSpaceTargetPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Packages/com.chocopoi.vrc.avatarlib/Prefabs/WorldSpaceTarget.prefab");
+
+            ParentConstraint worldSpaceParentConstraint = worldSpaceObj.AddComponent<ParentConstraint>();
+            worldSpaceParentConstraint.AddSource(new ConstraintSource()
+            {
+                sourceTransform = worldSpaceTargetPrefab.transform,
+                weight = 1
+            });
+            worldSpaceParentConstraint.constraintActive = true;
+
+            // Container
+
+            GameObject container = new GameObject("Container");
+            container.transform.SetParent(worldSpaceObj.transform);
+
+            // Reset target 
+
+            if (resetTarget == null)
+            {
+                GameObject resetTargetObj = new GameObject("ResetTarget");
+
+                if (root != null)
+                {
+                    resetTargetObj.transform.SetParent(root);
+                }
+
+                resetTarget = resetTargetObj.transform;
+            }
+
+            ParentConstraint resetTargetParentConstraint = container.AddComponent<ParentConstraint>();
+            resetTargetParentConstraint.AddSource(new ConstraintSource()
+            {
+                sourceTransform = resetTarget,
+                weight = 1
+            });
+            resetTargetParentConstraint.constraintActive = true;
+
+            return new WorldConstraint()
+            {
+                WorldSpaceConstraint = worldSpaceParentConstraint,
+                ResetTargetConstraint = resetTargetParentConstraint,
+                Root = worldSpaceObj,
+                Container = container
+            };
+        }
+
+        /// <summary>
+        /// Generates an animation to lock the world constraint to world space
+        /// </summary>
+        /// <param name="worldConstraint">The world constraint object</param>
+        /// <returns>An AnimationClip</returns>
+        private static AnimationClip GenerateLockAnimation(WorldConstraint worldConstraint)
+        {
+            AnimationClip clip = new AnimationClip();
+            AnimationUtils.SetSingleFrameComponentEnabledCurves(clip, new Component[] { worldConstraint.ResetTargetConstraint }, false);
+            return clip;
+        }
+
+        /// <summary>
+        /// Generates an animation to unlock the world constraint from world space
+        /// </summary>
+        /// <param name="worldConstraint">The world constraint object</param>
+        /// <returns>An AnimationClip</returns>
+        private static AnimationClip GenerateUnlockAnimation(WorldConstraint worldConstraint)
+        {
+            AnimationClip clip = new AnimationClip();
+            AnimationUtils.SetSingleFrameComponentEnabledCurves(clip, new Component[] { worldConstraint.ResetTargetConstraint }, true);
+            return clip;
+        }
+
+        /// <summary>
+        /// Generates an animation to set the world constraint root to be active (shown)
+        /// </summary>
+        /// <param name="worldConstraint">The world constraint object</param>
+        /// <returns>An AnimationClip</returns>
+        private static AnimationClip GenerateShowAnimation(WorldConstraint worldConstraint)
+        {
+            AnimationClip clip = new AnimationClip();
+            AnimationUtils.SetSingleFrameGameObjectEnabledCurves(clip, new GameObject[] { worldConstraint.Root }, true);
+            return clip;
+        }
+
+        /// <summary>
+        /// Generates an animation to set the world constraint root to be inactive (hidden)
+        /// </summary>
+        /// <param name="worldConstraint">The world constraint object</param>
+        /// <returns>An AnimationClip</returns>
+        private static AnimationClip GenerateHideAnimation(WorldConstraint worldConstraint)
+        {
+            AnimationClip clip = new AnimationClip();
+            AnimationUtils.SetSingleFrameGameObjectEnabledCurves(clip, new GameObject[] { worldConstraint.Root }, false);
+            return clip;
+        }
+    }
+}
