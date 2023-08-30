2fd71e7d4bf2f0a9386e165165af7c032329bdb5
poi-vrc <poi-vrc@users.noreply.github.com>
2023-08-30 02:43:42 +0000

    deploy: 7f62c3b22c8aab14642c65134bdbda34326295c6

--- search/files_4.js
+++ search/files_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['readme_2emd_0',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]]
+  ['setupscript_2ecs_0',['SetupScript.cs',['../_setup_script_8cs.html',1,'']]]
 ];
--- search/searchdata.js
+++ search/searchdata.js
@@ -3,11 +3,10 @@ var indexSectionsWithContent =
   0: "abcegimprsw",
   1: "aempsw",
   2: "c",
-  3: "aemprsw",
+  3: "aempsw",
   4: "abcegimprs",
   5: "s",
-  6: "crw",
-  7: "a"
+  6: "crw"
 };
 
 var indexSectionNames =
@@ -18,8 +17,7 @@ var indexSectionNames =
   3: "files",
   4: "functions",
   5: "variables",
-  6: "properties",
-  7: "pages"
+  6: "properties"
 };
 
 var indexSectionLabels =
@@ -30,7 +28,6 @@ var indexSectionLabels =
   3: "Files",
   4: "Functions",
   5: "Variables",
-  6: "Properties",
-  7: "Pages"
+  6: "Properties"
 };
 
