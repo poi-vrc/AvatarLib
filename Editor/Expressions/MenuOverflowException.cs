8e5a4b532ebf975939545dec2a9215844a064a3b
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2023-09-04 23:07:49 +0800

    chore: change to alpha 5

---
+++ Editor/Expressions/MenuOverflowException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#if VRC_SDK_VRCSDK3
+namespace Chocopoi.AvatarLib.Expressions
+{
+    public class MenuOverflowException : Exception
+    {
+        public MenuOverflowException()
+        {
+
+        }
+
+        public MenuOverflowException(string controlName, int amount) : base(string.Format("Cannot add more controls ({0}) to the current menu with {1} controls already!", controlName, amount))
+        {
+
+        }
+    }
+}
+#endif
---
+++ Editor/WorldConstraint/WorldConstraint.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+
+namespace Chocopoi.AvatarLib.WorldConstraint
+{
+    /// <summary>
+    /// A class referencing to the important objects of a world constraint
+    /// </summary>
+    public class WorldConstraint
+    {
+        /// <summary>
+        /// The ParentConstraint linking to the world space transform.
+        /// </summary>
+        public ParentConstraint WorldSpaceConstraint { get; set; }
+
+        /// <summary>
+        /// The ParentConstraint linking to the avatar transform. Set it to be inactive to leave the container in the world space.
+        /// </summary>
+        public ParentConstraint ResetTargetConstraint { get; set; }
+
+        /// <summary>
+        /// The world constraint root
+        /// </summary>
+        public GameObject Root { get; set; }
+
+        /// <summary>
+        /// The world constraint container to place objects into it
+        /// </summary>
+        public GameObject Container { get; set; }
+    }
+}
