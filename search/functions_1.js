341024ae7a47d4ae03f94bca3061fa4b857f4828
poi-vrc <poi-vrc@users.noreply.github.com>
2023-09-12 01:54:13 +0000

    deploy: a1743b371c30013ea700830328a380c04db7a846

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['beginnewsubmenu_0',['BeginNewSubMenu',['../class_chocopoi_1_1_avatar_lib_1_1_expressions_1_1_expression_menu_builder.html#a22a7371ee72c2cc3d2065559353869e4',1,'Chocopoi.AvatarLib.Expressions.ExpressionMenuBuilder.BeginNewSubMenu(string name, string parameterOnOpen, float valueOnOpen=1)'],['../class_chocopoi_1_1_avatar_lib_1_1_expressions_1_1_expression_menu_builder.html#a24d4cde9514157d28ed6f50634e7746b',1,'Chocopoi.AvatarLib.Expressions.ExpressionMenuBuilder.BeginNewSubMenu(string name, VRCExpressionsMenu.Control.Parameter parameterOnOpen=null, float valueOnOpen=1)']]]
+  ['beginnewsubmenu_0',['BeginNewSubMenu',['../class_chocopoi_1_1_avatar_lib_1_1_expressions_1_1_expression_menu_builder.html#ad13e35b38232382c114ef18c3dd58851',1,'Chocopoi::AvatarLib::Expressions::ExpressionMenuBuilder']]]
 ];
